45ecec978ee66ceaf19fd5bd3d691e15af63dff9
urawa72 <urawa72@gmail.com>
2020-05-14 16:19:05 +0900

    update setup linux

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,24 +1,40 @@
-#!/bin/bash
+#!/bin/sh
 
 echo Make sym links
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+export SCRIPT_DIR=$(pwd)
 ln -fs "$SCRIPT_DIR/bash/bashrc" "$HOME/.bashrc"
 ln -fs "$SCRIPT_DIR/bash/bash_profile" "$HOME/.bash_profile"
 ln -fs "$SCRIPT_DIR/vimrc" "$HOME/.vimrc"
 
 echo Install dependencies
-apt-get update
-apt-get install -y libxmu-dev
+apt update
+apt install -y \
+  vim-python \
+  python-dev \
+  python3-dev \
+  ruby-dev \
+  lua5.2 \
+  liblua5.2-dev \
+  luajit \
+  libluajit-5.1 \
+  autoconf \
+  automake \
+  cproto
 
 echo Build Vim
+cd /tmp
 git clone https://github.com/vim/vim
 cd vim
-./configure \
-  --with-x \
-  --enable-fail-if-missing \
-  --with-features=huge \
-  --disable-selinux \
-  --enable-cscope \
-  --enable-fontset \
-  --enable-multibyte
+ ./configure \
+   --with-features=huge \
+   --with-x \
+   --enable-multibyte \
+   --enable-cscope \
+   --enable-fontset \
+   --enable-fail-if-missing \
+   --prefix=/usr/local \
+   --enable-pythoninterp=dynamic \
+   --enable-python3interp=dynamic \
+   --enable-rubyinterp=dynamic \
 make && sudo make install
