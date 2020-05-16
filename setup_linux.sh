f26832a71bdb951a6d17c72b21cf308e1a21b28a
urawa72 <urawa72@gmail.com>
2020-05-16 11:06:34 +0900

    update setup linux

--- setup_linux.sh
+++ setup_linux.sh
@@ -2,7 +2,6 @@
 
 echo Make sym links
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
-export SCRIPT_DIR=$(pwd)
 ln -fs "$SCRIPT_DIR/bash/bashrc" "$HOME/.bashrc"
 ln -fs "$SCRIPT_DIR/bash/bash_profile" "$HOME/.bash_profile"
 ln -fs "$SCRIPT_DIR/vimrc" "$HOME/.vimrc"
@@ -31,14 +30,14 @@ echo Build Vim
 cd /tmp
 git clone https://github.com/vim/vim
 cd vim
- ./configure \
-   --with-features=huge \
-   --with-x \
-   --enable-multibyte \
-   --enable-cscope \
-   --enable-fontset \
-   --enable-fail-if-missing \
-   --enable-pythoninterp=dynamic \
-   --enable-python3interp=dynamic \
-   --enable-rubyinterp=dynamic \
-make && sudo make install
+./configure \
+  --with-features=huge \
+  --with-x \
+  --enable-multibyte \
+  --enable-cscope \
+  --enable-fontset \
+  --enable-fail-if-missing \
+  --enable-pythoninterp=dynamic \
+  --enable-python3interp=dynamic \
+  --enable-rubyinterp=dynamic \
+make && make install
