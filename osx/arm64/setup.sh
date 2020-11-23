595b72d88df5699e04d73e48063f96326a8ec4ae
urawa72 <urawa72@gmail.com>
2020-11-23 23:31:36 +0900

    add zshrc, vimrc for arm64

---
+++ osx/arm64/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ln -fs "$HOME/dotfiles/osx/arm64/zshrc" "$HOME/.zshrc"
+ln -fs "$HOME/dotfiles/osx/arm64/vimrc" "$HOME/.vimrc"
+ln -fs "$HOME/dotfiles/vim/coc/coc-settings.json" "$HOME/.config/nvim/coc-settings.json"
+ln -fs "$HOME/dotfiles/vim/coc/package.json" "$HOME/.config/coc/extensions/package.json"
