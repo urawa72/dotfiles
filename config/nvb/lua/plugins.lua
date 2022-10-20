ec1d12842586f28214e4dea67b0615e9ab412718
urawa72 <urawa72@gmail.com>
2022-10-20 22:53:12 +0900

    add nvb autopairs

--- config/nvb/lua/plugins.lua
+++ config/nvb/lua/plugins.lua
@@ -240,6 +240,33 @@ function M.setup()
 			},
 		}
 
+		-- Auto pairs
+		use {
+			"windwp/nvim-autopairs",
+			wants = "nvim-treesitter",
+			module = { "nvim-autopairs.completion.cmp", "nvim-autopairs" },
+			config = function()
+				require("config.autopairs").setup()
+			end,
+		}
+
+		-- Auto tag
+		use {
+			"windwp/nvim-ts-autotag",
+			wants = "nvim-treesitter",
+			event = "InsertEnter",
+			config = function()
+				require("nvim-ts-autotag").setup { enable = true }
+			end,
+		}
+
+		-- End wise
+		use {
+			"RRethy/nvim-treesitter-endwise",
+			wants = "nvim-treesitter",
+			event = "InsertEnter",
+		}
+
     if packer_bootstrap then
       print "Restart Neovim required after installation!"
       require("packer").sync()
