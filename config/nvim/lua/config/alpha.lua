082379645333b9437ce135135bb8c299afbfb6b8
urawa72 <urawa72@gmail.com>
2024-12-03 21:57:33 +0900

    update start display menu

--- config/nvim/lua/config/alpha.lua
+++ config/nvim/lua/config/alpha.lua
@@ -44,16 +44,16 @@ function M.setup()
 	dashboard.section.header.val = header()
 
 	dashboard.section.buttons.val = {
+		dashboard.button("f", "  Search", ":FzfLua files <CR>"),
 		dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
 		dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
-		dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 	}
 
 	local function footer()
 		-- Number of plugins
 		local total_plugins = #vim.tbl_keys(packer_plugins)
-		local datetime = os.date("%d-%m-%Y  %H:%M:%S")
-		local plugins_text = "\t" .. total_plugins .. " plugins  " .. datetime
+		local datetime = os.date("%d-%m-%Y %H:%M:%S")
+		local plugins_text = "\t" .. total_plugins .. " plugins " .. datetime
 
 		return plugins_text
 
