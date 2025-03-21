7fd5b4a1d646899f6cf3c115d045b37eb6a67411
urawa72 <urawa72@gmail.com>
2025-03-21 09:21:18 +0900

    remove startup command

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,10 +1,3 @@
 require("options")
 require("keymaps")
 require("plugins").setup()
-
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    vim.cmd("terminal")
-    vim.cmd("startinsert")
-  end
-})
